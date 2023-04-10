d9a7f629871b163e79678f0a6889004a4b349241
Wender Freese <wender.jean@gmail.com>
2023-04-10 10:52:22 -0300

    Improve nvim bufferline configuration

--- nvim/lua/user/bufferline.lua
+++ nvim/lua/user/bufferline.lua
@@ -5,6 +5,50 @@ end
 
 bufferline.setup({
     options = {
-        numbers = "none"
+        numbers = "none",
+        close_command = "Bdelete! %d",
+        modified_icon = "●",
+    },
+    highlights = {
+        fill = {
+            fg = { attribute = "fg", highlight = "#ff0000" },
+            bg = { attribute = "bg", highlight = "TabLine" },
+        },
+        background = {
+            fg = { attribute = "fg", highlight = "TabLine" },
+            bg = { attribute = "bg", highlight = "TabLine" },
+        },
+        close_button = {
+            fg = { attribute = "fg", highlight = "TabLine" },
+            bg = { attribute = "bg", highlight = "TabLine" },
+        },
+        close_button_visible = {
+            fg = { attribute = "fg", highlight = "TabLine" },
+            bg = { attribute = "bg", highlight = "TabLine" },
+        },
+        tab_selected = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "Normal" },
+        },
+        tab = {
+            fg = { attribute = "fg", highlight = "TabLine" },
+            bg = { attribute = "bg", highlight = "TabLine" },
+        },
+        tab_close = {
+            fg = { attribute = "fg", highlight = "TabLineSel" },
+            bg = { attribute = "bg", highlight = "Normal" },
+        },
+        modified = {
+            fg = { attribute = "fg", highlight = "TabLine" },
+            bg = { attribute = "bg", highlight = "TabLine" },
+        },
+        modified_selected = {
+            fg = { attribute = "fg", highlight = "Normal" },
+            bg = { attribute = "bg", highlight = "Normal" },
+        },
+        modified_visible = {
+            fg = { attribute = "fg", highlight = "TabLine" },
+            bg = { attribute = "bg", highlight = "TabLine" },
+        },
     }
 })
